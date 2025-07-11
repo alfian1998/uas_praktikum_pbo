84fc3f35442d9fa24b6949ec894482f4063dfbde
alfian1998 <alfian.muhammad1998@gmail.com>
2025-07-11 22:38:59 +0700

    initial

---
+++ src/main/java/com/mycompany/tugas_uas/Tugas_uas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas_uas;
+
+/**
+ *
+ * @author NoobMaster
+ */
+public class Tugas_uas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
